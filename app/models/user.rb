cbe8c5783bfda39204789c954be09c4386a24d54
drmcastles <drmcast@yandex.ru>
2021-04-22 02:17:05 +0300

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     def finished_tests(level)
-    Test.joins('join results on tests.id = results.test_id')
-    .where("results.user_id = #{ self.id } and tests.level = #{ level }")
+      Test.joins('join results on tests.id = results.test_id')
+      .where("results.user_id = #{ self.id } and tests.level = #{ level }")
 
     Result.joins(query)
     end
