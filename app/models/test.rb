3a2d35bc1842d7bdba7ebdcde491a544894f4cd0
drmcastles <drmcast@yandex.ru>
2021-04-21 16:42:08 +0300

    test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,8 @@
 class Test < ApplicationRecord
+  def self.by_category(category)
+  query = "JOIN categories
+           ON tests.category_id = categories.id
+           WHERE categories.title = #{ category }"
+
+  self.joins(query).order('title desc')
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  def finished_tests(level)
+		query = "JOIN tests ON tests.id = results.test_id
+						 WHERE user_id = #{ self.id } and tests.level = #{ level }"
+		Result.joins(query)
 end
