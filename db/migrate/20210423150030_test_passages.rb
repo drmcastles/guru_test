e17f3fb09a1b91f08f0b42b39c979a211ca2205a
drmcastles <drmcast@yandex.ru>
2021-04-23 18:19:34 +0300

    fix

--- db/migrate/20210423150030_test_passages.rb
+++ db/migrate/20210423150030_test_passages.rb
@@ -1,4 +1,9 @@
 class TestPassages < ActiveRecord::Migration[6.1]
   def change
+    create_table :test_passages do |t|
+     t.references :user, null: false, foreign_key: true
+     t.references :test, null: false, foreign_key: true
+
+     t.timestamps
   end
 end
